5637e8561486c780f1f43af650ceb67ecc2a9bf2
Jonathan SAHM <contact@johnstyle.fr>
2015-03-21 23:51:38 +0100

    Fix selectors

--- src/Johnstyle/pQuery/pQuery.php
+++ src/Johnstyle/pQuery/pQuery.php
@@ -10,29 +10,35 @@ namespace Johnstyle\pQuery;
  */
 class pQuery
 {
+    /** @var string $html */
     private $html;
-    private $currentHtml;
+
+    /** @var array $matches */
+    private $matches = array();
+
+    /** @var int $index */
+    private $index = 0;
 
     /**
-     * @param $html
+     * @param string $html
      */
     public function __construct ($html)
     {
-        $this->html = $html;
-        $this->currentHtml = $html;
+        $this->setHtml($html);
     }
 
     /**
-     * @param  bool $attribute
-     * @return bool
+     * @param  string $attribute
+     * @return string
      */
-    public function attr ($attribute = false)
+    public function attr ($attribute)
     {
-        if(preg_match("#^<[^>]+" . $attribute . "=(['\"])(.*?)\\1[^>]*>#si", $this->currentHtml, $match)) {
-            return $match[2];
+        if(preg_match("#^<[^>]+" . $attribute . "=(['\"])(.*?)\\1[^>]*>#si", $this->prepareHtml(), $match)) {
+
+            return trim($match[2]);
         }
 
-        return false;
+        return null;
     }
 
     /**
@@ -40,33 +46,44 @@ class pQuery
      */
     public function text ()
     {
-        return strip_tags($this->html());
+        return trim(strip_tags($this->html()));
     }
 
     /**
-     * @return bool
+     * @return string
      */
     public function html ()
     {
-        if(preg_match("#^<[^>]+>(.*)</[^>]+>$#si", $this->currentHtml, $match)) {
-            return $match[1];
+        if(preg_match("#^<[^>]+>(.*)</[^>]+>$#si", $this->prepareHtml(), $match)) {
+
+            return trim($match[1]);
         }
 
-        return false;
+        return null;
+    }
+
+    /**
+     * @param  int $index
+     * @return $this
+     */
+    public function eq ($index)
+    {
+        $this->index = (int) $index;
+
+        return $this;
     }
 
     /**
      * @param  string $selector
-     * @param  bool   $currentHtml
+     * @param  string $html
      * @return $this
      */
-    public function find ($selector, $currentHtml = false)
+    public function find ($selector, $html = null)
     {
-        if(!$currentHtml) {
+        if(is_null($html)) {
 
-            $this->setUnic();
-
-            $currentHtml = $this->currentHtml;
+            $this->matches = array();
+            $html = $this->html;
         }
 
         $selector = trim($selector);
@@ -82,39 +99,54 @@ class pQuery
             $subSelector = false;
 
             if(isset($match[2]) && $match[2]) {
+
                 $attribute = $match[2];
             }
 
             if(isset($match[3]) && $match[3]) {
+
                 $operator = $match[3];
+
             } elseif(isset($match[6]) && $match[6]) {
+
                 $operator = $match[6];
+
                 switch($operator) {
+
                     case '.':
+
                         $attribute = 'class';
                         break;
+
                     case '#':
+
                         $attribute = 'id';
                         break;
                 }
             }
 
             if(isset($match[4]) && $match[4]) {
+
                 $value = $match[4];
                 $value = str_replace('"', '\\"', $value);
+
             } elseif(isset($match[7]) && $match[7]) {
+
                 $value = $match[7];
             }
 
             if(isset($match[8]) && $match[8]) {
+
                 $extensionName = $match[8];
             }
 
             if(isset($match[9]) && $match[9]) {
+
                 $extensionValue = $match[9];
             }
 
             if(isset($match[10]) && $match[10]) {
+
                 $subSelector = $match[10];
             }
 
@@ -127,109 +159,158 @@ class pQuery
                 if($value) {
 
                     switch($operator) {
+
                         case '.':
+
                             $value = '(?:|.*\s)' . $value . '(?:|\s.*)';
                             break;
+
                         case '^':
+
                             $value = $value . '.+?';
                             break;
+
                         case '$':
+
                             $value = '.+?' . $value;
                             break;
+
                         case '*':
+
                             $value = '.*?' . $value . '.*?';
                             break;
+
                         case '!':
+
                             //$value = '(?:(?<!' . $value . ').+?|.+?(?!' . $value . '))';
                             $value = '(?!' . $value . ')';
                             break;
                     }
 
                 } else {
+
                     $value = '.*?';
                 }
 
                 $regex .= '\s*=\s*([\'"])' . $value . '\2';
+
             } else {
+
                 $regex .= '()';
             }
 
             $regex .= '(?:|\s+[^>]+|\s*/)>';
             $regex .= '(.*?)</' . $tagName . '\1>';
 
-            if (preg_match_all("#" . $regex . "#si", $currentHtml, $matches)) {
+            if (preg_match_all("#" . $regex . "#si", $html, $matches)) {
 
                 foreach($matches[0] as $i => $item) {
 
-                    $continue = false;
                     switch($extensionName) {
-                       default:
-                            $continue = true;
-                            break;
+
                         case 'eq':
-                            if($i == $extensionValue) {
-                                $continue = true;
+
+                            if($i !== (int) $extensionValue) {
+
+                                continue(2);
                             }
                             break;
+
                         case 'first':
-                            if($i == 0) {
-                                $continue = true;
+
+                            if($i !== 0) {
+
+                                break(2);
                             }
                             break;
+
                         case 'last':
-                            if($i == (count($matches[0]) - 1)) {
-                                $continue = true;
+
+                            if($i !== (count($matches[0]) - 1)) {
+
+                                continue(2);
                             }
                             break;
-
                     }
 
-                    if($continue) {
-                        if($subSelector) {
-                            if($this->find($subSelector, $matches[3][$i])) {
-                                break;
-                            }
-                        } else {
-                            $this->currentHtml = $item;
-                            break;
-                        }
+                    if(false !== $subSelector) {
+
+                        $this->find($subSelector, $matches[3][$i]);
+                        break;
                     }
+
+                    array_push($this->matches, $item);
                 }
 
-                return $this;
+            } else {
+
+                $this->html = null;
             }
         }
 
-        return false;
+        return $this;
     }
 
     /**
-     *
+     * @param  callable $callback
+     * @return $this
      */
-    private function setUnic()
+    public function each(callable $callback)
+    {
+        $matches = $this->matches;
+
+        foreach($matches as $match) {
+
+            $this->html = $match;
+
+            call_user_func_array($callback, array(&$this));
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param  string $html
+     * @return $this
+     */
+    private function setHtml($html)
     {
         $counter = array();
 
-        $this->currentHtml = preg_replace_callback("#((<)([a-z]+)(\s[^>]*|)(/?>)|(</)([a-z]+)(>))#si", function ($match) use(&$counter) {
+        $this->html = preg_replace_callback("#((<)([a-z]+)(\s[^>]*|)(/?>)|(</)([a-z]+)(>))#si", function ($match) use(&$counter) {
 
-                if(isset($match[7])) {
+            if(isset($match[7])) {
 
-                    $counter[$match[7]]--;
+                $counter[$match[7]]--;
 
-                    $tagName = $match[6] . $match[7] . '\\' . $counter[$match[7]] . $match[8];
+                $tagName = $match[6] . $match[7] . '\\' . $counter[$match[7]] . $match[8];
 
-                } else {
+            } else {
 
-                    if(!isset($counter[$match[3]])) {
-                        $counter[$match[3]] = 0;
-                    }
+                if(!isset($counter[$match[3]])) {
 
-                    $tagName = $match[2] . $match[3] . '\\' . $counter[$match[3]] . $match[4] . $match[5];
-
-                    $counter[$match[3]]++;
+                    $counter[$match[3]] = 0;
                 }
 
-                return $tagName;
-            }, $this->currentHtml);
+                $tagName = $match[2] . $match[3] . '\\' . $counter[$match[3]] . $match[4] . $match[5];
+
+                $counter[$match[3]]++;
+            }
+
+            return $tagName;
+
+        }, $html);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    private function prepareHtml()
+    {
+        $html = isset($this->matches[$this->index]) ? $this->matches[$this->index] : null;
+
+        return trim(preg_replace('#(</?[a-z]+)\\\\[0-9+](\s|/?>)#si', '$1$2', $html));
     }
 }
